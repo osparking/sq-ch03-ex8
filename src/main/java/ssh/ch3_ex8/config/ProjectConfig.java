1523c4e7017f8a4be34ddd43f08bc949923ac54e
osparking <jbpark03@gmail.com>
2023-08-31 23:09:20 +0900

    choose 1 out of many - 2 ways

--- src/main/java/ssh/ch3_ex8/config/ProjectConfig.java
+++ src/main/java/ssh/ch3_ex8/config/ProjectConfig.java
@@ -1,7 +1,9 @@
 package ssh.ch3_ex8.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import ssh.ch3_ex8.entity.Parrot;
 import ssh.ch3_ex8.entity.Person;
@@ -10,6 +12,7 @@ import ssh.ch3_ex8.entity.Person;
 public class ProjectConfig {
 
 	@Bean
+	@Primary
 	Parrot parrot1() {
 		var p = new Parrot();
 		p.setName("루키");
@@ -24,10 +27,10 @@ public class ProjectConfig {
 	}
 
 	@Bean
-	Person person(Parrot pa) {
+	Person person(@Qualifier("parrot2") Parrot parrot) {
 		var pe = new Person();
 		pe.setName("영수");
-		pe.setParrot(pa);
+		pe.setParrot(parrot);
 		return pe;
 	}
 }
